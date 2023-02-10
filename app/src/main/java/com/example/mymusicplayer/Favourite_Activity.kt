92ec7325e273f9acdf7381315eae2a25ca2e80cd
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2023-02-11 01:34:09 +0600

    Creating favourite & playlist activity & initialize binding

---
+++ app/src/main/java/com/example/mymusicplayer/Favourite_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.mymusicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Favourite_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourite)
+    }
+}
---
+++ app/src/main/java/com/example/mymusicplayer/Playlist_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.mymusicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Playlist_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist)
+    }
+}

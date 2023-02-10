230a92d703bdd5875c2c8da67441a40a2fe9dbe1
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2023-02-11 01:41:06 +0600

    Connecting the activities to the buttons using intents

--- app/src/main/java/com/example/mymusicplayer/Favourite_Activity.kt
+++ app/src/main/java/com/example/mymusicplayer/Favourite_Activity.kt
@@ -1,11 +1,17 @@
 package com.example.mymusicplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mymusicplayer.databinding.ActivityFavouriteBinding
+
 
 class Favourite_Activity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFavouriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favourite)
+        setTheme(androidx.appcompat.R.style.Theme_AppCompat_DayNight)
+        binding= ActivityFavouriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/mymusicplayer/PlayerActivity.kt
+++ app/src/main/java/com/example/mymusicplayer/PlayerActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mymusicplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mymusicplayer.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
+
+      private lateinit var binding: ActivityPlayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        setTheme(androidx.constraintlayout.widget.R.style.Theme_AppCompat_DayNight)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

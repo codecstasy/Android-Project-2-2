ee1cddf00aa7584dbcd10ab338155284b16c593d
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2023-02-16 12:35:09 +0600

    Loading external audio  files

--- app/src/main/java/com/example/mymusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/mymusicplayer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.mymusicplayer
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -10,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mymusicplayer.databinding.ActivityMainBinding
+import java.io.File
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var musicAdapter: MusicAdapter
+
+    companion object{
+        lateinit var MusicListMA : ArrayList<Music>
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -86,6 +93,7 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    @SuppressLint("SetTextI18n")
     private fun initializeLayout(){
         requestRuntimePermission()
         setTheme(R.style.Theme_MyMusicPlayer)
@@ -97,18 +105,48 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val musicList = ArrayList<String>()
-        musicList.add("1 song ")
-        musicList.add("2 song ")
-        musicList.add("3 song ")
-        musicList.add("4 song ")
-        musicList.add("5 song ")
+        MusicListMA = getAllAudio()
         binding.musicRV.setHasFixedSize(true)
         binding.musicRV.setItemViewCacheSize(14)
         binding.musicRV.layoutManager = LinearLayoutManager(this)
-        musicAdapter = MusicAdapter(this,musicList)
+        musicAdapter = MusicAdapter(this, MusicListMA)
         binding.musicRV.adapter = musicAdapter
         binding.totalSongs.text = "Total Songs : "+musicAdapter.itemCount
 
+    }
+
+
+   // loading audio from external files
+    @SuppressLint("Recycle", "Range", "SuspiciousIndentation")
+    private fun getAllAudio (): ArrayList<Music>{
+        val tempList = ArrayList<Music>()
+        val selection = MediaStore.Audio.Media.IS_MUSIC + " !=0"
+        val projection = arrayOf(MediaStore.Audio.Media._ID,MediaStore.Audio.Media.TITLE,MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.ARTIST,MediaStore.Audio.Media.DURATION,MediaStore.Audio.Media.DATE_ADDED,MediaStore.Audio.Media.DATA)
+           val cursor = this.contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection,selection,null,
+               MediaStore.Audio.Media.DATE_ADDED + " DESC",null)
+
+            if(cursor != null) {
+                if (cursor.moveToFirst())
+                    do {
+
+                        val titleC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
+                        val idC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID))
+                        val albumC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
+                        val artistC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+                        val pathC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
+                        val durationC = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
+                        val music = Music(id= idC,title=titleC, album = albumC, artist = artistC, path = pathC, duration = durationC)
+                        val file = File(music.path)
+                        if(file.exists())
+                            tempList.add(music)
+
+                    } while (cursor.moveToNext())
+                    cursor.close()
+
+            }
+
+        return tempList
     }
 
 

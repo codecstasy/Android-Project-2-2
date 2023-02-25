51bbda320de2d127d550817e1ef0ced62b936708
IftekharAnamArnob <arnob5363@gmail.com>
2023-02-25 19:43:24 +0600

    Formating song duration

--- app/src/main/java/com/example/mymusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/mymusicplayer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mymusicplayer
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.MenuItem
@@ -122,7 +123,7 @@ class MainActivity : AppCompatActivity() {
         val tempList = ArrayList<Music>()
         val selection = MediaStore.Audio.Media.IS_MUSIC + " !=0"
         val projection = arrayOf(MediaStore.Audio.Media._ID,MediaStore.Audio.Media.TITLE,MediaStore.Audio.Media.ALBUM,
-            MediaStore.Audio.Media.ARTIST,MediaStore.Audio.Media.DURATION,MediaStore.Audio.Media.DATE_ADDED,MediaStore.Audio.Media.DATA)
+            MediaStore.Audio.Media.ARTIST,MediaStore.Audio.Media.DURATION,MediaStore.Audio.Media.DATE_ADDED,MediaStore.Audio.Media.DATA,MediaStore.Audio.Media.ALBUM_ID)
            val cursor = this.contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection,selection,null,
                MediaStore.Audio.Media.DATE_ADDED + " DESC",null)
 
@@ -136,7 +137,11 @@ class MainActivity : AppCompatActivity() {
                         val artistC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
                         val pathC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
                         val durationC = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
-                        val music = Music(id= idC,title=titleC, album = albumC, artist = artistC, path = pathC, duration = durationC)
+                        val albumIdC = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)).toString()
+                        val uri = Uri.parse("content://media/external/audio/albumart")
+                        val artUriC = Uri.withAppendedPath(uri, albumIdC).toString()
+                        val music = Music(id= idC,title=titleC, album = albumC, artist = artistC, path = pathC, duration = durationC,
+                        artUri = artUriC)
                         val file = File(music.path)
                         if(file.exists())
                             tempList.add(music)
